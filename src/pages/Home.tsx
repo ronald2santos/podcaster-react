0920b81aa3d19784f0fd7057a076f76f2f40da48
ronald2santos <ronaldfigo2@gmail.com>
2023-03-16 11:43:51 +0100

    Made api call to get podcasts, created type for server data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
-
+import { useEffect, useState } from "react";
+import { PodcastServerList } from "../types";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const allowOriginURL = "https://api.allorigins.win/get?url=";
+  const baseUrl =
+    "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json";
+  const [top100Podcasts, setTop100Podcasts] = useState<PodcastServerList[]>([]);
 
-export default Home
+  const getTopPodcasts = async () => {
+    try {
+      const response = await fetch(
+        allowOriginURL + encodeURIComponent(baseUrl)
+      );
+      const results = await response.json();
+      const data = JSON.parse(results.contents);
+      const podcasts: PodcastServerList[] = data.feed.entry;
+      console.log(podcasts)
+      setTop100Podcasts(podcasts);
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTopPodcasts();
+  }, []);
+
+  return <div>Home</div>;
+};
+
+export default Home;
---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+type PodcastServerList = {
+    "im:artist": Object<any>;
+    "im:name": Object<any>;
+    "im:image": Object<any>;
+    summary: Object<any>;
+    id: Object<any>;
+};
+
+export {PodcastServerList}
